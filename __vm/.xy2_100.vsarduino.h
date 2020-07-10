a8c5c3c2d30c46d0881e4c104a4c54aa4345bd89
tp <txpzyr@126.com>
2020-07-10 16:06:21 +0800

    添加项目文件。

---
+++ __vm/.xy2_100.vsarduino.h
@@ -0,0 +1,95 @@
+/* 
+	Editor: https://www.visualmicro.com/
+			This file is for intellisense purpose only.
+			Visual micro (and the arduino ide) ignore this code during compilation. This code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			The contents of the _vm sub folder can be deleted prior to publishing a project
+			All non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			Note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+	
+	Hardware: ESP32 Dev Module, Platform=arduino-esp32, Package=espressif
+*/
+
+#if defined(_VMICRO_INTELLISENSE)
+
+#ifndef _VSARDUINO_H_
+#define _VSARDUINO_H_
+#define ESP_PLATFORM
+#define HAVE_CONFIG_H
+#define GCC_NOT_5_2_0 0
+#define WITH_POSIX
+#define F_CPU 240000000L
+#define ARDUINO 108010
+#define ARDUINO_ESP32_DEV
+#define ESP32
+#define CORE_DEBUG_LEVEL 5
+#define __cplusplus 201103L
+#define __inline__
+#define __asm__(x)
+#define __extension__
+#define __ATTR_PURE__
+#define __ATTR_CONST__
+#define __inline__
+#define __volatile__
+
+
+#define __ICCARM__
+#define __ASM
+#define __INLINE
+#define __builtin_va_list void
+//#define _GNU_SOURCE 
+//#define __GNUC__ 0
+//#undef  __ICCARM__
+//#define __GNU__
+
+typedef long Pio;
+typedef long Efc;
+typedef long Adc;
+typedef long Pwm;
+typedef long Rtc;
+typedef long Rtt;
+typedef long pRtc;
+typedef long Spi;
+typedef long spi;
+typedef long Ssc;
+//typedef long p_scc;
+typedef long Tc;
+//typedef long pTc;
+typedef long Twi;
+typedef long Wdt;
+//typedef long pTwi;
+typedef long Usart;
+typedef long Pdc;
+typedef long Rstc;
+
+extern const int ADC_MR_TRGEN_DIS = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG0 = 0;
+extern const int ADC_MR_TRGSEL_Pos = 0;
+
+extern const int ADC_MR_TRGSEL_Msk = 0;
+extern const int ADC_MR_TRGEN = 0;
+extern const int ADC_TRIG_TIO_CH_0 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG1 = 0;
+extern const int ADC_TRIG_TIO_CH_1 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG2 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG3 = 0;
+
+#define __ARMCC_VERSION 400678
+#define __attribute__(noinline)
+
+#define prog_void
+#define PGM_VOID_P int
+
+
+            
+typedef unsigned char byte;
+extern "C" void __cxa_pure_virtual() {;}
+
+
+
+#include "arduino.h"
+#include <pins_arduino.h> 
+#undef cli
+#define cli()
+#include "xy2_100.ino"
+#endif
+#endif
